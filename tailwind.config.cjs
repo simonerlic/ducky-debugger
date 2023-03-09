85f39dbd88174e238fe533f6a65afa0d4550718b
Simon Erlic <simon@erlic.ca>
2023-03-09 14:14:47 -0800

    Add monaco, create columns, tutorial message

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend:
+		{
+			fontFamily: {
+				'sans': ['Work Sans', 'sans-serif'],
+				'spacemono': ['Space Mono', 'monospace'],
+			},
+		},
 	},
 	plugins: [],
 }
