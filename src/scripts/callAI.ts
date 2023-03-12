2adac74daa54678792955d6336438815bf333ae2
Simon Erlic <simon@erlic.ca>
2023-03-11 22:30:39 -0800

    Temporary work in adding API (will remove)

--- src/scripts/callAI.ts
+++ src/scripts/callAI.ts
@@ -1,7 +1,13 @@
 // Grab text from monaco and the text from the input box, then send it to the AI
 import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api.js';
+import { Configuration, OpenAIApi } from "openai";
+import node from '@astrojs/node';
 
-function callAI() {
+var messages = [
+    {role: "System", content: "You are an AI rubber duck debugger. You listen to a user's expanation of their code, and point out the lines where a flaw is found."}
+];
+
+async function callAI() {
     const monacoText = monacoEditor.editor.getModels()[0].getValue();
     // print out the text in the monaco editor
     console.log(monacoText);
@@ -17,27 +23,19 @@ function callAI() {
     newP.innerHTML = "User: " + text;
     outputDiv.appendChild(newP);
 
+    newP.scrollIntoView()
 
-    // const fullText = monacoText + " " + text;
-    // const aiText = document.getElementById("aiText") as HTMLInputElement;
-    // aiText.value = "Thinking...";
-    // const url = "http://localhost:5000/api/ai";
-    // const data = { text: fullText, };
-    // fetch(url, {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(data),
-    // })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         console.log("Success:", data);
-    //         aiText.value = data.text;
-    //     })
-    //     .catch((error) => {
-    //         console.error("Error:", error);
-    //     });
+    const configuration = new Configuration({
+        apiKey: 'YOUR_API_KEY',
+    });
+    const openai = new OpenAIApi(configuration);
+      
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages as any,
+    });
+    console.log(completion.data.choices[0].message);
 }
 
 export default callAI;
+export { messages };
