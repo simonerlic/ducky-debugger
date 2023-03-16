dace0f9cac97f74f596c3d310464a2798f837340
Simon Erlic <simon@erlic.ca>
2023-03-16 14:19:50 -0700

    Working on astro API stuff

--- src/pages/api/completion.ts
+++ src/pages/api/completion.ts
@@ -2,14 +2,39 @@
 import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
 
+dotenv.config();
+
 let messages = [
     {role: "System", content: "You are an AI rubber duck debugger. You listen to a user's expanation of their code, and point out the lines where a flaw is found."}
 ];
 
+// Path: src/pages/api/completion.ts
+export async function post({ request }: { request: any }) {
+    // Take data from the request, add it to the messages array, and send it to the AI
+    const body = await request.json();
+
+    // messages.push({role: "User", content: body.text});
+
+    // Call the AI
+    let resp = await callAI();
+
+    console.log(resp.data.choices[0].message?.content!)
+
+    // Add the AI's response to the messages array
+    // messages.push({role: "System", content: resp.data.choices[0].message?.content!});
+
+    console.log(messages);
+    return {
+        body: JSON.stringify({
+          message: 'Hello from Astro!',
+        }),
+    };
+}
+
 async function callAI() {
 
     const configuration = new Configuration({
-        apiKey: dotenv.config().parsed!.OPENAI_API_KEY,
+        apiKey: process.env.OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
       
@@ -18,30 +43,7 @@ async function callAI() {
         messages: messages as any,
     });
 
-    console.log(completion.data.choices[0].message);
-
-    return completion.data.choices[0].message;
+    return completion;
 
     // return "This is a test response";
-}
-
-// Path: src/pages/api/completion.ts
-export async function post({request}) {
-    // Take data from the request, add it to the messages array, and send it to the AI
-    const body = await request.json();
-    messages.push({role: "User", content: body.text});
-
-    // Call the AI
-    let resp = await callAI();
-
-    // Add the AI's response to the messages array
-    messages.push({role: "System", content: resp});
-
-    console.log(messages);
-    return {
-        body: JSON.stringify({
-          name: 'Astro',
-          url: 'https://astro.build/',
-        }),
-    };
 }
