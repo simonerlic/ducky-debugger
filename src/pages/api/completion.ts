2861a70654f4e3bdc66d7263dc074b0a114d2d29
Simon Erlic <simon@erlic.ca>
2023-03-17 13:06:52 -0700

    Fix API calling

--- src/pages/api/completion.ts
+++ src/pages/api/completion.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 let messages = [
-    {role: "System", content: "You are an AI rubber duck debugger. You listen to a user's expanation of their code, and point out the lines where a flaw is found."}
+    {role: "system", content: "You are an AI rubber duck debugger. You listen to a user's expanation of their code, and point out the lines where a flaw is found."}
 ];
 
 // Path: src/pages/api/completion.ts
@@ -13,7 +13,7 @@ export async function post({ request }: { request: any }) {
     // Take data from the request, add it to the messages array, and send it to the AI
     const body = await request.json();
 
-    // messages.push({role: "User", content: body.text});
+    messages.push({role: "user", content: body.text});
 
     // Call the AI
     let resp = await callAI();
@@ -21,9 +21,8 @@ export async function post({ request }: { request: any }) {
     console.log(resp.data.choices[0].message?.content!)
 
     // Add the AI's response to the messages array
-    // messages.push({role: "System", content: resp.data.choices[0].message?.content!});
+    messages.push({role: "System", content: resp.data.choices[0].message?.content!});
 
-    console.log(messages);
     return {
         body: JSON.stringify({
           message: 'Hello from Astro!',
@@ -32,18 +31,16 @@ export async function post({ request }: { request: any }) {
 }
 
 async function callAI() {
-
     const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY,
+        organization: "org-wr95k3KKxaBYkVZtK9FfTFAL",
+        apiKey: process.env.OPENAI_API,
     });
     const openai = new OpenAIApi(configuration);
-      
+
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: messages as any,
+        messages: messages,
     });
 
     return completion;
-
-    // return "This is a test response";
 }
